911c0310fcba7ccf8e5b246cba12894013594d88
rdprassy <rdprassy@gmail.com>
2020-03-24 23:45:01 +0530

    added welcome component

--- frontend/todo/src/app/app.component.ts
+++ frontend/todo/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  template: '<h1>{{title}}<h1>',
+  templateUrl: './app.component.html',
+  // template: '<h1>{{title}}<h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
