beb74c3a16f3865b74b91aa6bf657f6717fd6f82
rdprassy <rdprassy@gmail.com>
2020-03-28 15:41:27 +0530

    added route from login to welcome

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,16 +13,17 @@ export class LoginComponent implements OnInit {
   errormessage = 'Invalid user'
   invalidLogin = false
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
-
+ 
   // event data binding
   handleLogin(){
 
     if(this.username ==="rdprassy" && this.password === 'rdprassy'){
+      this.router.navigate(['welcome'])
       this.invalidLogin = false
     }
     else{
