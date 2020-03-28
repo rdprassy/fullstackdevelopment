0735b72cf3a7a3a3456fa8ac8ada12864beab28d
rdprassy <rdprassy@gmail.com>
2020-03-28 16:04:51 +0530

    added todo component and introduced ngFor

---
+++ frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-todos',
+  templateUrl: './list-todos.component.html',
+  styleUrls: ['./list-todos.component.css']
+})
+export class ListTodosComponent implements OnInit {
+
+  todos = [
+    {
+      id:1,
+      description: 'Learn to Dance'
+
+    },
+    {
+      id:2,
+      description: 'Learn to Code'
+    }
+
+]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
