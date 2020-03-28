3104eae6fdf6bc5b1cc83cfa211a7b2628936a65
rdprassy <rdprassy@gmail.com>
2020-03-28 16:19:19 +0530

    modified the way the todo datastructure to be a class and redesigned it

--- frontend/todo/src/app/list-todos/list-todos.component.ts
+++ frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo{
+
+  constructor(
+    public id:number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+    ) {
+
+    }
+}
+
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,15 +21,8 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodosComponent implements OnInit {
 
   todos = [
-    {
-      id:1,
-      description: 'Learn to Dance'
-
-    },
-    {
-      id:2,
-      description: 'Learn to Code'
-    }
+   new Todo(1,'learn to Dance',false,new Date()),
+   new Todo(2,'Learn to Code',false,new Date())
 
 ]
 
