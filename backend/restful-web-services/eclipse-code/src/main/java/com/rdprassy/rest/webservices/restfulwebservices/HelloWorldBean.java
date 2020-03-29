be09e99bd7c27301c0f8f1b7aff829182eaf1fb9
rdprassy <rdprassy@gmail.com>
2020-03-29 22:14:46 +0530

    added rest endpoints for path variables and bean

---
+++ backend/restful-web-services/eclipse-code/src/main/java/com/rdprassy/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,26 @@
+package com.rdprassy.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		// TODO Auto-generated constructor stub
+		
+		this.setMessage(message);
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+
+}
--- backend/restful-web-services/eclipse-code/src/main/java/com/rdprassy/rest/webservices/restfulwebservices/HelloWorldController.java
+++ backend/restful-web-services/eclipse-code/src/main/java/com/rdprassy/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.rdprassy.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,22 @@ public class HelloWorldController {
 	public String helloWorld() {
 		
 		return "Hello World";
+		
+	}
+	
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		
+		return new HelloWorldBean("Hello World");
+		
+	}
+	
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		
+		return new HelloWorldBean(String.format("Hello World,%s",name));
 		
 	}
 
