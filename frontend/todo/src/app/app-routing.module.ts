8f1aa1e827d738638ce35c18d970bba3c4f66dcb
rdprassy <rdprassy@gmail.com>
2020-03-29 21:19:33 +0530

    added a route guard service to make some routes avail/unavail on conditions

--- frontend/todo/src/app/app-routing.module.ts
+++ frontend/todo/src/app/app-routing.module.ts
@@ -5,16 +5,17 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
 import { LogoutComponent } from './logout/logout.component';
+import { RouteGuardService } from './service/route-guard.service';
 
 
 const routes: Routes = [
   
 {path:'', component: LoginComponent},
 
-{path:'login', component: LoginComponent},
-{path:'welcome/:name', component: WelcomeComponent},
-{path:'todos', component: ListTodosComponent},
-{path:'logout', component: LogoutComponent},
+{path:'login', component: LoginComponent},  //canActivate, RouteGuardService
+{path:'welcome/:name', component: WelcomeComponent, canActivate:[RouteGuardService]},
+{path:'todos', component: ListTodosComponent, canActivate:[RouteGuardService]},
+{path:'logout', component: LogoutComponent, canActivate:[RouteGuardService]},
 {path:'**', component: ErrorComponent}
 
 
