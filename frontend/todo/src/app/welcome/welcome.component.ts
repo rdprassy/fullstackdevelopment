2c3df15f7f3cd8cb57440eb7bc5d93796efb1d9f
rdprassy <rdprassy@gmail.com>
2020-04-11 15:03:20 +0530

    for calling a webservice in angular
    for calling a webservice in angular
    1. create a button
    2. create a service which should be called on button click.
    3. call the httpClient thing module added in appmodule.ts with get request to url such that you allow cross origins from spring boot
    4. ensure the subsribe method is called on the httpclient get request.

--- frontend/todo/src/app/welcome/welcome.component.ts
+++ frontend/todo/src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 // import {AppComponent } from '../app.component';
 
 @Component({
@@ -11,12 +12,21 @@ export class WelcomeComponent implements OnInit {
 
   name = ''
 
-  constructor(private route:ActivatedRoute) { }
+  constructor(
+    private route:ActivatedRoute,
+    private service:WelcomeDataService
+    ) { }
 
   ngOnInit() {
 
     console.log(this.route.snapshot.params['name'])
     this.name = this.route.snapshot.params['name']
+  }
+
+  getWelcomeMessage(){
+    console.log(this.service.executeHelloWorldBeanService());
+    this.service.executeHelloWorldBeanService().subscribe();
+    console.log("Get Welcome Message");
   }
 
 }
