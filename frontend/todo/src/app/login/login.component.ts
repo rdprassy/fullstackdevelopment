96802b1b473454bc33f528c4a2050164b28ca6dd
rdprassy <rdprassy@gmail.com>
2020-04-25 15:15:49 +0530

    ensured now the authentication is taken care by the backend service provided by the spring boot

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
+import { BasicAuthenticationService } from '../service/basic-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +15,8 @@ export class LoginComponent implements OnInit {
   errormessage = 'Invalid user'
   invalidLogin = false
 
-  constructor(private router: Router, private hardcodedAuthenticationService:HardcodedAuthenticationService) { }
+  constructor(private router: Router, private hardcodedAuthenticationService:HardcodedAuthenticationService,
+    private basicAuthenticationService:BasicAuthenticationService) { }
 
   ngOnInit() {
   }
@@ -36,6 +38,27 @@ export class LoginComponent implements OnInit {
       this.invalidLogin =  true
     }
     
+
+//console.log(this.username)
+
+  }
+
+  handleBasicLogin(){
+
+    this.basicAuthenticationService.executeAuthenticatinService(this.username,this.password)
+    .subscribe(
+      data => {
+        console.log(data)
+        this.router.navigate(['welcome',this.username])
+        this.invalidLogin = false
+      },
+      error => {
+        console.log(error)
+        this.invalidLogin = true;
+      }
+     )
+     
+   
 
 //console.log(this.username)
 
---
+++ frontend/todo/src/app/service/basic-authentication.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+import { LogoutComponent } from '../logout/logout.component';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasicAuthenticationService {
+
+  constructor(private http:HttpClient ) { }
+
+  authenticate(username,password){
+
+    console.log('before login '+this.isUserLoggedIn())
+
+    if(username ==="rdprassy" && password === 'rdprassy'){
+
+      sessionStorage.setItem('authenticateduser',username)
+      console.log('after login '+this.isUserLoggedIn())
+
+     return true;
+    }
+    return false; 
+
+  }
+
+  executeAuthenticatinService(username,password){
+
+  let basicAuthHeaderString = 'Basic '+ window.btoa(username +':' + password)
+    let headers = new HttpHeaders({
+      Authorization: basicAuthHeaderString
+    })
+    console.log(name)
+    return this.http.get<AuthenticationBean>(`http://localhost:8080/basicauth`,{headers: headers})
+    .pipe(
+      map(
+        data =>{
+
+      sessionStorage.setItem('authenticateduser',username);
+      return data;
+        }
+      )
+    );
+
+}
+
+createBasicAuthenticationHttpHeader(){
+  let username = 'rdprassy';
+  let password = 'rdprassy';
+  let basicAuthHeaderString = 'Basic '+ window.btoa(username +':' + password)
+  return basicAuthHeaderString;
+}
+
+  isUserLoggedIn(){
+    let user= sessionStorage.getItem('authenticateduser')
+    return !(user===null)
+  }
+
+  logout(){
+
+    sessionStorage.removeItem('authenticateduser')
+
+
+  }
+}
+
+
+export class AuthenticationBean{
+
+
+  constructor(public messge:string){
+
+  }
+}
+
+
